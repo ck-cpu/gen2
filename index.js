bae8fa9e7de942bb1244ddabe50f102e10e1f36d
ck-cpu <kevinsh525@gmail.com>
2021-04-22 19:01:35 +0900

    ㅇ

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config()
-const token = precess.env.tokens;
+const token = process.env.token;
 
 client.on('ready', () => {
   console.log('켰다.');
