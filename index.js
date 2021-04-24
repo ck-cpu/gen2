24cc21015915acb86f684f04051a9a7ade6e521b
ck-cpu <kevinsh525@gmail.com>
2021-04-24 17:35:15 +0900

    d

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
+const token = process.env.token;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
